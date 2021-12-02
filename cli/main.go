09963108cd0a6f5c497ad998af96da51a7de9a83
Mohammed Nafees <hello@mnafees.me>
2021-12-02 21:09:59 +0530

    quick fix to move ApplyOpts outside of internal

--- cli/main.go
+++ cli/main.go
@@ -6,11 +6,11 @@ import (
 	"os"
 
 	"github.com/fatih/color"
-	"github.com/porter-dev/switchboard/internal/exec"
 	"github.com/porter-dev/switchboard/pkg/drivers/helm"
 	"github.com/porter-dev/switchboard/pkg/drivers/kubernetes"
 	"github.com/porter-dev/switchboard/pkg/drivers/terraform"
 	"github.com/porter-dev/switchboard/pkg/parser"
+	"github.com/porter-dev/switchboard/pkg/types"
 	"github.com/porter-dev/switchboard/pkg/worker"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
@@ -81,7 +81,7 @@ func apply(args []string, logger *zerolog.Logger) error {
 	worker.RegisterDriver("kubernetes", kubernetes.NewKubernetesDriver)
 	worker.RegisterDriver("terraform", terraform.NewTerraformDriver)
 
-	return worker.Apply(resGroup, &exec.ApplyOpts{
+	return worker.Apply(resGroup, &types.ApplyOpts{
 		BasePath: basePath,
 	})
 }
