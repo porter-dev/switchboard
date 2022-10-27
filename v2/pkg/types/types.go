c94bb98e53980e85325b555dfd56d9aaebd3e1e0
Mohammed Nafees <hello@mnafees.me>
2022-10-27 16:16:56 -0400

    apparently json tags dont work for yaml parser, but they claim to support it

--- v2/pkg/types/types.go
+++ v2/pkg/types/types.go
@@ -1,67 +1,67 @@
 package types
 
 type Variable struct {
-	Name   *YAMLNode[string] `json:"name" validate:"required,unique"`
-	Value  *YAMLNode[string] `json:"value" validate:"required_if=Random false"`
-	Once   *YAMLNode[bool]   `json:"once"`
-	Random *YAMLNode[bool]   `json:"random"`
-	Length *YAMLNode[uint]   `json:"length"`
+	Name   *YAMLNode[string] `yaml:"name" validate:"required,unique"`
+	Value  *YAMLNode[string] `yaml:"value" validate:"required_if=Random false"`
+	Once   *YAMLNode[bool]   `yaml:"once"`
+	Random *YAMLNode[bool]   `yaml:"random"`
+	Length *YAMLNode[uint]   `yaml:"length"`
 }
 
 type EnvGroup struct {
-	Name      *YAMLNode[string] `json:"name" validate:"required"`
-	CloneFrom *YAMLNode[string] `json:"clone_from" validate:"required"`
+	Name      *YAMLNode[string] `yaml:"name" validate:"required"`
+	CloneFrom *YAMLNode[string] `yaml:"clone_from" validate:"required"`
 }
 
 type Build struct {
-	Name       *YAMLNode[string]                       `json:"name" validate:"required"`
-	Context    *YAMLNode[string]                       `json:"context" validate:"dir"`
-	Method     *YAMLNode[string]                       `json:"method" validate:"required,oneof=pack docker registry"`
-	Builder    *YAMLNode[string]                       `json:"builder" validate:"required_if=Method pack"`
-	Buildpacks []*YAMLNode[string]                     `json:"buildpacks"`
-	Dockerfile *YAMLNode[string]                       `json:"dockerfile" validate:"required_if=Method docker"`
-	Image      *YAMLNode[string]                       `json:"image" validate:"required_if=Method registry"`
-	Env        map[*YAMLNode[string]]*YAMLNode[string] `json:"env"`
-	EnvGroups  []*YAMLNode[string]                     `json:"env_groups"`
+	Name       *YAMLNode[string]                       `yaml:"name" validate:"required"`
+	Context    *YAMLNode[string]                       `yaml:"context" validate:"dir"`
+	Method     *YAMLNode[string]                       `yaml:"method" validate:"required,oneof=pack docker registry"`
+	Builder    *YAMLNode[string]                       `yaml:"builder" validate:"required_if=Method pack"`
+	Buildpacks []*YAMLNode[string]                     `yaml:"buildpacks"`
+	Dockerfile *YAMLNode[string]                       `yaml:"dockerfile" validate:"required_if=Method docker"`
+	Image      *YAMLNode[string]                       `yaml:"image" validate:"required_if=Method registry"`
+	Env        map[*YAMLNode[string]]*YAMLNode[string] `yaml:"env"`
+	EnvGroups  []*YAMLNode[string]                     `yaml:"env_groups"`
 }
 
 type AddonResource struct {
-	Name      *YAMLNode[string]                    `json:"name" validate:"required,unique"`
-	DependsOn []*YAMLNode[string]                  `json:"depends_on"`
-	Type      *YAMLNode[string]                    `json:"type" validate:"required"`
-	ChartURL  *YAMLNode[string]                    `json:"chart_url" validate:"url"`
-	Version   *YAMLNode[string]                    `json:"version"`
-	Deploy    map[*YAMLNode[string]]*YAMLNode[any] `json:"deploy"`
+	Name      *YAMLNode[string]                    `yaml:"name" validate:"required,unique"`
+	DependsOn []*YAMLNode[string]                  `yaml:"depends_on"`
+	Type      *YAMLNode[string]                    `yaml:"type" validate:"required"`
+	ChartURL  *YAMLNode[string]                    `yaml:"chart_url" validate:"url"`
+	Version   *YAMLNode[string]                    `yaml:"version"`
+	Deploy    map[*YAMLNode[string]]*YAMLNode[any] `yaml:"deploy"`
 }
 
 // type AppResourceBuild struct {
-// 	Name       string         `json:"name" mapstructure:"name"`
-// 	Context    string         `json:"context" mapstructure:"context"`
-// 	Method     string         `json:"method" mapstructure:"method"`
-// 	Builder    string         `json:"builder" mapstructure:"builder"`
-// 	Buildpacks []string       `json:"buildpacks" mapstructure:"buildpacks"`
-// 	Dockerfile string         `json:"dockerfile" mapstructure:"dockerfile"`
-// 	Image      string         `json:"image" mapstructure:"image"`
-// 	Env        map[string]any `json:"env" mapstructure:"env"`
-// 	EnvGroups  []string       `json:"env_groups" mapstructure:"env_groups"`
-// 	Ref        string         `json:"ref" mapstructure:"ref"`
+// 	Name       string         `yaml:"name" mapstructure:"name"`
+// 	Context    string         `yaml:"context" mapstructure:"context"`
+// 	Method     string         `yaml:"method" mapstructure:"method"`
+// 	Builder    string         `yaml:"builder" mapstructure:"builder"`
+// 	Buildpacks []string       `yaml:"buildpacks" mapstructure:"buildpacks"`
+// 	Dockerfile string         `yaml:"dockerfile" mapstructure:"dockerfile"`
+// 	Image      string         `yaml:"image" mapstructure:"image"`
+// 	Env        map[string]any `yaml:"env" mapstructure:"env"`
+// 	EnvGroups  []string       `yaml:"env_groups" mapstructure:"env_groups"`
+// 	Ref        string         `yaml:"ref" mapstructure:"ref"`
 // }
 
 type AppResource struct {
-	Name      *YAMLNode[string]                    `json:"name" validate:"required,unique"`
-	DependsOn []*YAMLNode[string]                  `json:"depends_on"`
-	Type      *YAMLNode[string]                    `json:"type" validate:"required,oneof=web worker job"`
-	ChartURL  *YAMLNode[string]                    `json:"chart_url" validate:"url"`
-	Version   *YAMLNode[string]                    `json:"version"`
-	Deploy    map[*YAMLNode[string]]*YAMLNode[any] `json:"deploy"`
-	Build     map[*YAMLNode[string]]*YAMLNode[any] `json:"build"`
+	Name      *YAMLNode[string]                    `yaml:"name" validate:"required,unique"`
+	DependsOn []*YAMLNode[string]                  `yaml:"depends_on"`
+	Type      *YAMLNode[string]                    `yaml:"type" validate:"required,oneof=web worker job"`
+	ChartURL  *YAMLNode[string]                    `yaml:"chart_url" validate:"url"`
+	Version   *YAMLNode[string]                    `yaml:"version"`
+	Deploy    map[*YAMLNode[string]]*YAMLNode[any] `yaml:"deploy"`
+	Build     map[*YAMLNode[string]]*YAMLNode[any] `yaml:"build"`
 }
 
 type PorterYAML struct {
-	Version   *YAMLNode[string]           `json:"version" validate:"required"`
-	Variables *YAMLNode[[]*Variable]      `json:"variables"`
-	EnvGroups *YAMLNode[[]*EnvGroup]      `json:"env_groups"`
-	Builds    *YAMLNode[[]*Build]         `json:"builds"`
-	Apps      *YAMLNode[[]*AppResource]   `json:"apps"`
-	Addons    *YAMLNode[[]*AddonResource] `json:"addons"`
+	Version   *YAMLNode[string]           `yaml:"version" validate:"required"`
+	Variables *YAMLNode[[]*Variable]      `yaml:"variables"`
+	EnvGroups *YAMLNode[[]*EnvGroup]      `yaml:"env_groups"`
+	Builds    *YAMLNode[[]*Build]         `yaml:"builds"`
+	Apps      *YAMLNode[[]*AppResource]   `yaml:"apps"`
+	Addons    *YAMLNode[[]*AddonResource] `yaml:"addons"`
 }
